76482e2b529234fa8c7fba3b3dc17c09c5b028e4
Tovics <tovics1@gmail.com>
2017-05-11 10:42:31 +0200

    something...something...something darkside...

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    pass
+def home():
+    return render_template("template.html")
+
+
+@app.route("/get_cunt", methods=["GET"])
+def template():
+    get_cunt += 1
+    return render_template("template.html", get_cunt)
+
+
+def main():
+    get_cunt = 0
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
